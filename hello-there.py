ec99fff095bde1002f75ebf03f1b7c1138293678
Renzo Lucioni <renzo@lucioni.xyz>
2018-03-04 22:13:28 -0500

    async task execution

--- hello-there.py
+++ hello-there.py
@@ -1,6 +1,9 @@
 import os
+from time import sleep
 
+import requests
 from flask import abort, Flask, jsonify, request
+from zappa.async import task
 
 
 app = Flask(__name__)
@@ -13,12 +16,26 @@ def is_request_valid(request):
     return is_token_valid and is_team_id_valid
 
 
+@task
+def hello_there_task(response_url):
+    sleep(5)
+
+    data = {
+        'response_type': 'in_channel',
+        'text': 'You _are_ a bold one.',
+    }
+
+    requests.post(response_url, json=data)
+
+
 @app.route('/hello-there', methods=['POST'])
 def hello_there():
     if not is_request_valid(request):
         abort(400)
 
+    hello_there_task(request.form['response_url'])
+
     return jsonify(
         response_type='in_channel',
-        text='<https://youtu.be/rEq1Z0bjdwc|General Kenobi!>',
+        text='<https://youtu.be/frszEJb0aOo|General Kenobi!>',
     )
